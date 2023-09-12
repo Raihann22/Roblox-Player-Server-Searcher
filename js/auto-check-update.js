c92dc380c5f31a397078e5ca2bfba21ca9a07166
Raihann22 <betang46@gmail.com>
2023-09-12 21:52:24 +0800

    fix and organize

--- js/auto-check-update.js
+++ js/auto-check-update.js
@@ -1,29 +1,47 @@
 const githubOwner = "Raihann22";
 const githubRepo = "Roblox-Player-Server-Searcher";
 
-fetch(`https://api.github.com/repos/${githubOwner}/${githubRepo}/releases/latest`).then(x => x.json()).then(result => {
+chrome.storage.local.get(null, async (RPSS) => {
+    const currentDate = Math.floor(Date.now() / 1000);
+    const oneDay = (currentDate - RPSS.lastUpdateAlert) / (60 * 60) > 24;
+
+    /**
+     *  after user closing new update alert, it will show the alert again in 24 hours (if there is an update)
+     *  if its equal or more than 24 hours, it will check if there an update every 30 minutes
+     */
+    if (!oneDay || (currentDate - RPSS.lastCheckUpdate) / 60 < 30) return;
+
+    const availableVersion = await fetch(`https://api.github.com/repos/${githubOwner}/${githubRepo}/releases/latest`).then(async response => {
+        const isRateLimitExceeded = response.status === 403;
+        const rateLimitReset = Number(response.headers.get("x-ratelimit-reset")) - 1740;
+        const result = await response.json();
+        const version = result.tag_name;
+
+        RPSS.lastCheckUpdate = isRateLimitExceeded ? rateLimitReset : currentDate;
+        await chrome.storage.local.set(RPSS);
+
+        if (isRateLimitExceeded || !version) return;
+        return version.slice(1);
+    });
+
+    if (!availableVersion) return;
+
     const currentVersion = chrome.runtime.getManifest().version;
-    const availableVersion = result.tag_name.slice(1);
-
     if (currentVersion !== availableVersion) {
-        chrome.storage.local.get("RPSS", function (result) {
-            const currentDate = Date.now();
+        RPSS.lastCheckUpdate = 0;
+        await chrome.storage.local.set(RPSS);
 
-            if ((currentDate - result.RPSS.lastUpdateAlert) / (1000 * 60 * 60) > 24) { // only show once every 24 hours
-                showUpdate(currentVersion, availableVersion);
-
-                result.RPSS.lastUpdateAlert = currentDate;
-                chrome.storage.local.set({ "RPSS": result.RPSS });
-            }
+        const container = await showUpdate(currentVersion, availableVersion);
+        container.children[0].addEventListener("click", async () => {   // close alert
+            container.hidden = true;
+            RPSS.lastUpdateAlert = currentDate;
+            await chrome.storage.local.set(RPSS);
         });
     }
-}).catch(() => {
-    console.log("RPSS: Failed to check update!");
-})
+});
 
-// showUpdate("for", "test");
-function showUpdate(currentVersion, availableVersion) {
-    fetch(chrome.runtime.getURL("html/new-update.html")).then(result => result.text()).then(html => {
+async function showUpdate(currentVersion, availableVersion) {
+    return await fetch(chrome.runtime.getURL("html/new-update.html")).then(x => x.text()).then(html => {
         const updateAlert = document.createElement("div");
         const body = document.body;
         body.firstChild ? body.insertBefore(updateAlert, body.firstChild) : body.appendChild(updateAlert);
@@ -35,8 +53,6 @@ function showUpdate(currentVersion, availableVersion) {
         container.children[3].children[1].children[1].innerText = `v${availableVersion}`;                   // available version
         container.lastElementChild.href = `https://github.com/${githubOwner}/${githubRepo}#readme`;         // link to readme
 
-        container.children[0].addEventListener("click", () => {                                             // close alert
-            container.hidden = true;
-        });
+        return container;
     });
 }
