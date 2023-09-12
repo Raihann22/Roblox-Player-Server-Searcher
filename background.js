9344f86af97e680d0e78680092b8b75924cc05e6
Raihann22 <betang46@gmail.com>
2023-09-12 21:54:45 +0800

    [© 2022 ->© 2023] & fetch Presence api from here

--- background.js
+++ background.js
@@ -3,13 +3,21 @@
  * GitHub Repository: https://github.com/Raihann22/Roblox-Player-Server-Searcher
  * LICENSE: https://choosealicense.com/licenses/mit
  * 
- * © 2022 Raihan
+ * © 2023 Raihan
  */
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    const msg = message.action;
+    const msg = message.msg;
+    const content = message.content;
 
     switch (msg) {
+        case "RPSS_Run_auto-check-update.js":
+            chrome.scripting.executeScript({
+                target: { tabId: sender.tab.id, allFrames: false },
+                files: ["js/auto-check-update.js"]
+            });
+            break;
+
         case "RPSS_Run_wait-for-server-list-options.js":
             chrome.scripting.executeScript({
                 target: { tabId: sender.tab.id, allFrames: false },
@@ -30,5 +38,22 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                 files: ["js/searcher.js"]
             });
             break;
+
+        case "Fetch_PresenceApi":
+            fetchPresenceApi(content).then(sendResponse);
+            return true;
     }
 });
+
+async function fetchPresenceApi(userIds) {
+    const result = await fetch("https://presence.roblox.com/v1/presence/users", {
+        method: "POST",
+        headers: {
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ "userIds": [userIds] })
+    }).then(x => x.json());
+
+    return result.userPresences[0];
+}
