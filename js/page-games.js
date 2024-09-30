9b8dc70a6f4ed029be37531de11c31040811ee2b
Raihan <98381055+Raihann22@users.noreply.github.com>
2024-09-30 13:21:49 +0800

    13 doesnt search and gives error (#16)
    * fix rate limited when fetching servers with size more than 5 players
    
    * bump version
    
    ---------
    
    Co-authored-by: Raihann22 <betang46@gmail.com>

--- js/page-games.js
+++ js/page-games.js
@@ -1,4 +1,5 @@
 let PLACE_ID;
+let PLACE_UNIVERSE_ID;
 let PROGRESS_BAR;
 let JOIN_HTML;
 let JOIN_JS;
@@ -7,6 +8,7 @@ let PLAYER_IN_OTHER_GAME_JS;
 
 (async () => {
     PLACE_ID = Number((window.location.href).match(/\/games\/(\d+)\//)[1]);
+    PLACE_UNIVERSE_ID = document.getElementById("game-detail-meta-data").getAttribute("data-universe-id");
     JOIN_HTML = await fetch(chrome.runtime.getURL("resources/html/joining.html")).then(x => x.text());
     JOIN_JS = await import(chrome.runtime.getURL("resources/js/join.js"));
     PLAYER_IN_OTHER_GAME_HTML = await fetch(chrome.runtime.getURL("resources/html/player-in-other-game.html")).then(x => x.text());
